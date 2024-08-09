a9814eac62906c29796d8e3abbf39ad67cd92686
Grace <kenney.grace6@gmail.com>
2024-08-09 15:30:17 -0400

    add documentation to chr_pos_to_vrange

--- R/chr_pos_to_vrange.R
+++ R/chr_pos_to_vrange.R
@@ -1,6 +1,19 @@
-library(GenomicRanges)
-
-# function that makes vranges from genomic position
+#' Builds a VRanges object from genomic position and allele information
+#'
+#' @param chr character, chromosome identifier
+#' @param pos integer, SNP position on chromosome
+#' @param ref character, reference nucleotide (can be upper or lower)
+#' @param alt character, alternate nulceotide (can be upper or lower)
+#'
+#' @return a `VRanges` object the seqnames, ranges, ref, and alt columns
+#' filled with parameter options supplied.
+#'
+#' @examples
+#'
+#' vr <- VRanges(seqnames = "chr1", ranges = 15000, ref = "A", alt = "T")
+#' vr
+#'
+#' @export
 chr_pos_to_vrange <- function(chr, pos, ref, alt){
   vr <- VRanges(chr, ranges = pos,
                 ref = ref, alt = alt)
