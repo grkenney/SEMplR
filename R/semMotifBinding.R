ea404a3a7dd278a791b2cf59edcfb7b9ff6f4673
Grace <kenney.grace6@gmail.com>
2024-09-23 13:09:40 -0400

    create new scoreVariant function to improve speed

--- R/semMotifBinding.R
+++ R/semMotifBinding.R
@@ -1,6 +1,54 @@
 #' Define a function to calculate risk/non-risk binding propensity
 #' for all SEM motifs provided
 #'
+#' @param vr `VRanges` object with a single variant
+#' @param sem_matrix a numeric matrix of motif position by nucleic acid
+#' @param tf_name a string of the transcription factor name
+#' @param threshold a numeric value of the threshold to use for normalization
+scoreVariant <- \(vr, sem_matrix, tf_name, offset, threshold) {
+
+  ## Score risk and non-risk sequences against each sem
+  starting_indices <- (offset-nrow(sem_matrix)+2):(offset+1)
+  nonRiskScores <-
+    Biostrings::PWMscoreStartingAt(pwm = t(sem_matrix),
+                                   subject = vr$ref_seq,
+                                   starting.at = starting_indices)
+  riskScores <-
+    Biostrings::PWMscoreStartingAt(pwm = t(sem_matrix),
+                                   subject = vr$alt_seq,
+                                   starting.at = starting_indices)
+
+  ## Find which frame has the best binding score (and record this)
+  nonRiskFrame <- which.max(nonRiskScores)
+  riskFrame <- which.max(riskScores)
+
+  ref_seq_frame <- stringr::str_sub(vr$ref_seq,
+                                    start = starting_indices[nonRiskFrame],
+                                    end = starting_indices[nonRiskFrame]+nrow(sem_matrix))
+
+  alt_seq_frame <- stringr::str_sub(vr$alt_seq,
+                                    start = starting_indices[riskFrame],
+                                    end = starting_indices[riskFrame]+nrow(sem_matrix))
+
+  nonRiskNorm <- (2^nonRiskScores[nonRiskFrame] - threshold) / abs(threshold)
+  riskNorm <- (2^riskScores[riskFrame] - threshold) / abs(threshold)
+
+  scores <- data.table::data.table(seqnames=as.character(GenomeInfoDb::seqnames(vr)),
+                                   ranges=BiocGenerics::start(vr),
+                                   sem=tf_name,
+                                   nonRiskSeq=ref_seq_frame,
+                                   riskSeq=alt_seq_frame,
+                                   nonRiskScore=nonRiskScores[nonRiskFrame],
+                                   riskScore=riskScores[riskFrame],
+                                   nonRiskNorm=nonRiskNorm,
+                                   riskNorm=riskNorm)
+  return(scores)
+}
+
+
+#' Define a function to calculate risk/non-risk binding propensity
+#' for all SEM motifs provided
+#'
 #' @param vr `VRanges` object
 #' @param semList a list of `SNPEffectMatrix` objects. Defaults to loading .sems
 #' and baseline data from github.com/Boyle-Lab/SEMpl/raw/master/SEMs/
@@ -29,48 +77,18 @@ semMotifBinding <- \(vr,
   ## Create a new SEMplR object to store results
   semScores <- SemplR(vr)
 
+  semScores@scores <- data.table::data.table()
+
   for (i in 1:length(vr)){
-    for(j in 1:length(sems)) {
+    varScore <- lapply(1:length(semList),
+                       function(j) { scoreVariant(semScores@variants[i],
+                                                  semList[[j]]@matrix,
+                                                  semList[[j]]@tf_name,
+                                                  offset,
+                                                  threshold[[j]]) })
+    varScoreDt <- data.table::rbindlist(varScore)
 
-      sem_matrix <- sems[[j]]@matrix
-
-      ## Score risk and non-risk sequences against each sem
-      starting_indices <- (offset-nrow(sem_matrix)+2):(offset+1)
-      nonRiskScores <-
-        Biostrings::PWMscoreStartingAt(pwm = t(sem_matrix),
-                           subject = vr[i]$ref_seq,
-                           starting.at = starting_indices)
-      riskScores <-
-        Biostrings::PWMscoreStartingAt(pwm = t(sem_matrix),
-                           subject = vr[i]$alt_seq,
-                           starting.at = starting_indices)
-
-      ## Find which frame has the best binding score (and record this)
-      if (max(nonRiskScores) > max(riskScores)){
-        frame <- which.max(nonRiskScores)
-      } else {
-        frame <- which.max(riskScores)
-      }
-
-      ref_seq_frame <- stringr::str_sub(vr[i]$ref_seq,
-                                        start = starting_indices[frame],
-                                        end = starting_indices[frame]+nrow(sem_matrix))
-
-      alt_seq_frame <- stringr::str_sub(vr[i]$alt_seq,
-                                        start = starting_indices[frame],
-                                        end = starting_indices[frame]+nrow(sem_matrix))
-
-      semScores@scores <- rbind(semScores@scores,
-                                data.table::data.table(seqnames=as.character(GenomeInfoDb::seqnames(semScores@variants[i])),
-                                           ranges=BiocGenerics::start(semScores@variants[i]),
-                                           sem=sems[[j]]@tf_name,
-                                           nonRiskSeq=ref_seq_frame,
-                                           riskSeq=alt_seq_frame,
-                                           nonRiskScore=nonRiskScores[frame],
-                                           riskScore=riskScores[frame],
-                                           nonRiskNorm=(2^nonRiskScores[frame] - threshold[[j]]) / abs(threshold[[j]]),
-                                           riskNorm=(2^riskScores[frame] - threshold[[j]]) / abs(threshold[[j]])))
-    }
+    semScores@scores <- rbind(semScores@scores, varScoreDt)
   }
 
   ## Return
