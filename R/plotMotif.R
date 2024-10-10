a1c9dec4b2ff2dabe385080cdc9f311b7ccec334
Grace Kenney <gracekenney@Graces-MBP-226.lan>
2024-10-10 02:27:11 -0400

    add code for motif position plot

--- R/plotMotif.R
+++ R/plotMotif.R
@@ -1,33 +1,33 @@
-# sem_file_list <- list.files("/users/g/k/gkenney/semplr/testing_sempl/SEMpl/SEMs",
-#                             pattern = ".sem", full.names = T)
-# dt <- semMotifBinding(chr = "chr12",
-#                       pos = 94136009,
-#                       ref = "G",
-#                       alt = "C",
-#                       semFiles = sem_file_list,
-#                       semBaselines = "/users/g/k/gkenney/semplr/testing_sempl/SEMpl/SEMs/BASELINE/SEMs_baseline_norm.txt")
-#
-# psm <- plotSemMotifs(dt)
-#
-#
-# library(tidyr)
-#
-# sems <- lapply(sem_file_list, fread, header = TRUE)
-#
-# names(sems) <-
-#   lapply(sems, \(x) colnames(x)[[1]]) |>
-#   unlist() |>
-#   paste0("_", 1:length(sems))
-#
-# long_sems <- sems$JUN_162 %>%
-#   pivot_longer(cols=c(A, C, G, T), names_to="AA", values_to="score")
-#
-# ggplot(long_sems, aes(x=JUN, y=score, color=AA)) +
-#   geom_hline(yintercept=0, color = "grey", size = 2) +
-#   geom_hline(yintercept=-0.5, linetype="dashed", color = "grey", size = 2) + # this isn't exactly the right spot
-#   geom_text(aes(label=AA), size=8) +
-#   theme_classic() +
-#   theme(legend.position="none") +
-#   xlab("Position in JUN Motif") +
-#   ylab("SNP Effect Score") +
-#   theme(text = element_text(size = 20))
+#' Plot motif SEM scores
+#'
+#' @param sems list of SNPEffectMatrix objects
+#' @param motif sem id to plot
+#'
+#' @import ggplot2
+#'
+#' @return a `ggplot` with sem scores for each amino acid per position
+#'
+#' @export
+plotMotif <- function(sems, motif) {
+  sem_baseline <- sems[[motif]]@baseline
+  
+  sem_mtx <- sems[[motif]]@matrix
+  sem_mtx$pos <- rownames(sem_mtx)
+  
+  aa_text_size <- 6
+  
+  motif_plot <- ggplot2::ggplot(sem_mtx, aes(x=pos, y=A)) +
+    geom_hline(yintercept=0, color = "grey", size = 1) +
+    geom_hline(yintercept=sem_baseline, linetype="dashed", 
+               color = "grey", size = 1) + 
+    geom_text(aes(label="A"), size = aa_text_size, col="#D81B60") +
+    geom_text(aes(y = T, label="T"), size = aa_text_size, col="#1E88E5") +
+    geom_text(aes(y = C, label="C"), size = aa_text_size, col="#004D40") +
+    geom_text(aes(y = G, label="G"), size = aa_text_size, col="#A189D2") +
+    theme_classic() +
+    theme(legend.position="none") +
+    xlab("Position in Motif") +
+    ylab("SNP Effect Score") +
+    theme(text = element_text(size = 12))
+  return(motif_plot)
+}
