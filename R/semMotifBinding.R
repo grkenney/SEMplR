a5cb1965dad280a59ce1c7714a44264382aa878f
Grace Kenney <gracekenney@graces-mbp-226.wireless-1x.unc.edu>
2024-10-14 15:22:44 -0400

    score all variants together per sem rather than looping through them

--- R/semMotifBinding.R
+++ R/semMotifBinding.R
@@ -1,67 +1,44 @@
 #' Define a function to calculate risk/non-risk binding propensity
 #' for all SEM motifs provided
 #'
-#' @param vr `VRanges` object with a single variant
+#' @param vr `VRanges` object with ref_seq and alt_seq metadata columns
 #' @param sem_mtx a numeric matrix of motif position by nucleic acid
+#' @param bl a numeric value of the baseline to use for normalization
 #' @param offset maximum number of offset basepairs offset from variant
-#' @param baseline a numeric value of the baseline to use for normalization
-scoreSem <- \(vr, sem_mtx, offset, baseline) {
+scoreVariants <- function(var_id, var_seq, sem_mtx_id, sem_mtx, bl, offset) {
+  concat_seq <- paste(var_seq, collapse="")
+  nvars <- length(var_seq)
+  nframes <- nrow(sem_mtx)
   
-  ## Score risk and non-risk sequences against each sem
-  starts <- (offset-nrow(sem_mtx)+2):(offset+1)
+  var_starts <- (offset*2+1) * (0:(nvars-1))
   
-  ref_seq <- vr$ref_seq
-  alt_seq <- vr$alt_seq
+  frame_starts <- unlist( lapply( var_starts,
+                                  function(n) n + (offset-nframes+2):(offset+1) ) )
   
-  sem_mtx_t <- t(sem_mtx)
+  pwm_scores <- PWMscoreStartingAt(pwm = t(sem_mtx), 
+                                   subject = concat_seq, 
+                                   starting.at = frame_starts)
+  scores_mtx <- matrix(pwm_scores, nrow = nframes)
   
-  nonRiskScores <-
-    Biostrings::PWMscoreStartingAt(pwm = sem_mtx_t,
-                                   subject = ref_seq,
-                                   starting.at = starts)
+  score_max <- colMaxs(scores_mtx)
+  score_norm <- (2^score_max - 2^bl) / abs(2^bl)
   
-  riskScores <-
-    Biostrings::PWMscoreStartingAt(pwm = sem_mtx_t,
-                                   subject = alt_seq,
-                                   starting.at = starts)
+  score_max_i <- apply(scores_mtx, 2, which.max)
   
-  ## Find which frame has the best binding score (and record this)
-  nonRiskFrame <- which.max(nonRiskScores)
-  riskFrame <- which.max(riskScores)
-
-  nonRiskScore <- nonRiskScores[nonRiskFrame]
-  riskScore <- riskScores[riskFrame]
-
-  ref_seq_frame <- substr(ref_seq,
-                          start = starts[nonRiskFrame],
-                          stop = starts[nonRiskFrame]+nrow(sem_mtx))
-
-  alt_seq_frame <- substr(alt_seq,
-                          start = starts[riskFrame],
-                          stop = starts[riskFrame]+nrow(sem_mtx))
-
-
-  nonRiskNorm <- (2^nonRiskScore - baseline) / abs(baseline)
-  riskNorm <- (2^riskScore - baseline) / abs(baseline)
+  # get the sequence of the frame
+  seq_frame_start <- unlist(lapply(1:length(score_max_i), 
+                                   function(i) ( frame_starts[score_max_i[i] + (0:nvars * nframes)[i]] )))
   
-  scores <- data.table::data.table(nonRiskSeq=ref_seq_frame,
-                                   riskSeq=alt_seq_frame,
-                                   nonRiskScore=nonRiskScore,
-                                   riskScore=riskScore,
-                                   nonRiskNorm=nonRiskNorm,
-                                   riskNorm=riskNorm)
-  return(scores)
-}
-
-scoreVariant <- function(vr, semList) {
-  varScore <- lapply(semList,
-                     function(x) scoreSem(vr,
-                                          sem_matrix(x),
-                                          offset,
-                                          baseline(x)))
+  seq_frame <- unlist( lapply( seq_frame_start, 
+                               function(x) substr(concat_seq, x, x + nframes - 1) ))
   
-  varScoreDt <- data.table::rbindlist(varScore)
-  return(varScoreDt)
+  scores_dt <- data.table::data.table(var_id=var_id,
+                                      sem_mtx_id=sem_mtx_id,
+                                      seq=seq_frame,
+                                      score=score_max,
+                                      scoreNorm=score_norm)
+  
+  return(scores_dt)
 }
 
 
@@ -86,13 +63,22 @@ semMotifBinding <- \(vr, semList) {
   ## Create a new SemplScores object to store results
   semScores <- SemplScores(vr, sems = semList)
   
-  new_scores <- lapply(seq_along(vr), 
-                       function(i) scoreVariant(vr[i], semList)) |>
+  ref_scores <- lapply(semList,
+                       function(x) scoreVariants(vr$id, vr$ref_seq, sem_id(x), sem_matrix(x), baseline(x), offset) ) |>
+    rbindlist()
+  ref_scores_merge <- merge(ref_scores, scores(semScores), 
+                            by.x = c("var_id", "sem_mtx_id"), 
+                            by.y = c("varId", "sem"))
+  scores(semScores)[, c("nonRiskSeq", "nonRiskScore", "nonRiskNorm")] <- ref_scores_merge[, c("seq", "score", "scoreNorm")]
+
+  alt_scores <- lapply(semList,
+                       function(x) scoreVariants(vr$id, vr$alt_seq, sem_id(x), sem_matrix(x), baseline(x), offset) ) |>
     rbindlist()
   
-  scores(semScores)[, c("nonRiskSeq", "riskSeq", 
-                        "nonRiskScore", "riskScore", 
-                        "nonRiskNorm", "riskNorm")] <- new_scores
+  alt_scores_merge <- merge(alt_scores, scores(semScores), 
+                            by.x = c("var_id", "sem_mtx_id"), 
+                            by.y = c("varId", "sem"))
+  scores(semScores)[, c("riskSeq", "riskScore", "riskNorm")] <- alt_scores_merge[, c("seq", "score", "scoreNorm")]
 
   return(semScores)
 }
