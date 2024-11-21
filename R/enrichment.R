642a31d3884d45599d5e2b0850a563c5223fb6b7
Grace Kenney <gracekenney@Graces-MBP-226.lan>
2024-11-20 21:36:02 -0500

    change enrichment to Fisher's Exact Test

--- R/enrichment.R
+++ R/enrichment.R
@@ -1,163 +1,110 @@
-#' Permute sems for a given variant
-#' 
-#' @param sem_scores SemplScores object
-#' @param motif_count matrix with a row for each motif and a column for each
-#' motif change category (ie, gained, lost, changed, maintained)
-#' @param variant_id unique id for variant to be permuted
+#' Build a contingency table for gained motifs
 #'
-permuteVariant <- function(sem_scores, motif_count, variant_id) {
-  # randomize sem ids
-  sem_ids <- sem_scores@sem_metadata$sem_id
-  randomized_sem_ids <- sample(sem_ids, replace = F)
+#' @param s score table from a SemplScores object
+#' @param si SEM id
+gainedContingencyTable <- function(s, si) {
+  # Gained in SEM
+  a <- s[(nonRiskNorm < 0 & riskNorm > 0) & sem == si] |> nrow()
+  # Not Gained in SEM
+  b <- s[!(nonRiskNorm < 0 & riskNorm > 0) & sem == si] |> nrow()
+  # Gained not in SEM
+  c <- s[(nonRiskNorm < 0 & riskNorm > 0) & sem != si] |> nrow()
+  # Not Gained not in SEM
+  d <- s[!(nonRiskNorm < 0 & riskNorm > 0) & sem != si] |> nrow()
   
-  # subset scores to variant
-  variant_scores <- variantScores(sem_scores, variant_id)
-  
-  # apply randomized labels
-  variant_scores$sem <- randomized_sem_ids
-  
-  # count number of variants where each motif is gained/lost/maintained
-  gained_motifs <- changed_motif(variant_scores, "gained")$sem
-  motif_count[gained_motifs, "gained"] <- motif_count[gained_motifs, "gained"] + 1
-  
-  lost_motifs <- changed_motif(variant_scores, "lost")$sem
-  motif_count[lost_motifs, "lost"] <- motif_count[lost_motifs, "lost"] + 1
-  
-  changed_motifs <- changed_motif(variant_scores, "changed")$sem
-  motif_count[changed_motifs, "changed"] <- motif_count[changed_motifs, "changed"] + 1
-  
-  maintained_motifs <- changed_motif(variant_scores, "maintained")$sem
-  motif_count[maintained_motifs, "maintained"] <- motif_count[maintained_motifs, "maintained"] + 1
-  
-  return(motif_count)
+  contingencyTable <- matrix(c(a, c, 
+                               b, d), ncol=2)
+  return(contingencyTable)
 }
 
-#' Permute sems for a given variant
-#' 
-#' @param permutation_counts matrix storing permutation counts
-#' @param motif_count matrix with a row for each motif and a column for each
-#' motif change category (ie, gained, lost, changed, maintained)
-#' @param sem unique sem id
+#' Build a contingency table for lost motifs
 #'
-countAlteredMotifs <- function(permutation_counts, motif_count, sem) {
-  if (motif_count[sem, "gained"] >= permutation_counts[sem, "actual_gained"]) {
-    permutation_counts[sem, "perm_gt_gained"] <- permutation_counts[sem, "perm_gt_gained"] + 1
-  }
-  if (motif_count[sem, "lost"] >= permutation_counts[sem, "actual_lost"]) {
-    permutation_counts[sem, "perm_gt_lost"] <- permutation_counts[sem, "perm_gt_lost"] + 1
-  }
-  if (motif_count[sem, "changed"] >= permutation_counts[sem, "actual_changed"]) {
-    permutation_counts[sem, "perm_gt_changed"] <- permutation_counts[sem, "perm_gt_changed"] + 1
-  }
-  if (motif_count[sem, "maintained"] >= permutation_counts[sem, "actual_maintained"]) {
-    permutation_counts[sem, "perm_gt_maintained"] <- permutation_counts[sem, "perm_gt_maintained"] + 1
-  }
-  return(permutation_counts)
-}
-
-#' Permute sems for a given variant
-#' 
-#' @param permutation_counts matrix storing permutation counts
-#' @param num_permutations number of permutations to perform
-#'
-calculatePvalues <- function(permutation_counts, num_permutations) {
-  # calculate pvalue
-  permutation_counts[, "pval_gained"] <- unlist(lapply(permutation_counts[, "perm_gt_gained"], 
-                                                       function(x) x / num_permutations))
-  permutation_counts[, "pval_lost"] <- unlist(lapply(permutation_counts[, "perm_gt_lost"], 
-                                                     function(x) x / num_permutations))
-  permutation_counts[, "pval_changed"] <- unlist(lapply(permutation_counts[, "perm_gt_changed"], 
-                                                     function(x) x / num_permutations))
-  permutation_counts[, "pval_maintained"] <- unlist(lapply(permutation_counts[, "perm_gt_maintained"], 
-                                                           function(x) x / num_permutations))
+#' @param s score table from a SemplScores object
+#' @param si SEM id
+lostContingencyTable <- function(s, si) {
+  # Lost in SEM
+  a <- s[(nonRiskNorm > 0 & riskNorm < 0) & sem == si] |> nrow()
+  # Not Lost in SEM
+  b <- s[!(nonRiskNorm > 0 & riskNorm < 0) & sem == si] |> nrow()
+  # Lost not in SEM
+  c <- s[(nonRiskNorm > 0 & riskNorm < 0) & sem != si] |> nrow()
+  # Not Lost not in SEM
+  d <- s[!(nonRiskNorm > 0 & riskNorm < 0) & sem != si] |> nrow()
   
-  # calculated BH adjusted pvalue
-  permutation_counts[, "adjpval_gained"] <- unlist(lapply(permutation_counts[, "pval_gained"],
-                                                          function(x) p.adjust(x, method="BH")))
-  permutation_counts[, "adjpval_lost"] <- unlist(lapply(permutation_counts[, "pval_lost"],
-                                                        function(x) p.adjust(x, method="BH")))
-  permutation_counts[, "adjpval_changed"] <- unlist(lapply(permutation_counts[, "pval_changed"],
-                                                        function(x) p.adjust(x, method="BH")))
-  permutation_counts[, "adjpval_maintained"] <- unlist(lapply(permutation_counts[, "pval_maintained"],
-                                                              function(x) p.adjust(x, method="BH")))
-  return(permutation_counts)
+  contingencyTable <- matrix(c(a, c, 
+                               b, d), ncol=2)
+  return(contingencyTable)
 }
 
-#' Count the number of variants with altered motifs for each motif
-#' 
-#' @param sem_scores SemplScores object
-#' @param pc matrix storing permutation counts
-#' 
-#' @return pc matrix with actual counts populated
-tallyAlteredMotifs <- function(sem_scores, pc) {
-  for (s in rownames(pc)) {
-    m <- motifScores(sem_scores, s)
-    pc[s, "actual_gained"] <- nrow(changed_motif(m, "gained"))
-    pc[s, "actual_lost"] <- nrow(changed_motif(m, "lost"))
-    pc[s, "actual_changed"] <- nrow(changed_motif(m, "changed"))
-    pc[s, "actual_maintained"]  <- nrow(changed_motif(m, "maintained"))
+#' Build a contingency table for changed motifs
+#'
+#' @param s score table from a SemplScores object
+#' @param si SEM id
+changedContingencyTable <- function(s, si) {
+  # Lost in SEM
+  a <- s[((nonRiskNorm > 0 & riskNorm < 0) |
+           (nonRiskNorm < 0 & riskNorm > 0)) & sem == si] |> nrow()
+  # Not Lost in SEM
+  b <- s[!((nonRiskNorm > 0 & riskNorm < 0) |
+             (nonRiskNorm < 0 & riskNorm > 0)) & sem == si] |> nrow()
+  # Lost not in SEM
+  c <- s[((nonRiskNorm > 0 & riskNorm < 0) |
+            (nonRiskNorm < 0 & riskNorm > 0)) & sem != si] |> nrow()
+  # Not Lost not in SEM
+  d <- s[!((nonRiskNorm > 0 & riskNorm < 0) |
+             (nonRiskNorm < 0 & riskNorm > 0)) & sem != si] |> nrow()
+  
+  contingencyTable <- matrix(c(a, c, 
+                               b, d), ncol=2)
+  return(contingencyTable)
+}
+
+#' Return a contingency table according to direction of test
+#'
+#' @param s score table from a SemplScores object
+#' @param si SEM id
+#' @param d direction of test. Options are: "changed", "gained", "lost"
+getContingencyTable <- function(s, si, d) {
+  if (d == "changed") {
+    ct <- changedContingencyTable(s = s, si = si)
+  } else if (d == "gained") {
+    ct <- gainedContingencyTable(s = s, si = si)
+  } else if (d == "lost") {
+    ct <- lostContingencyTable(s = s, si = si)
+  } else {
+    stop("Invalid input for argument d. Must be 'changed', 'gained', or 'lost'")
   }
-  return(pc)
+  return(ct)
 }
 
-#' Perform a permuation test to find changed motifs among all variants analyzed
-#' 
-#' @param sem_scores SemplScores object that has already been scored with 
-#' semMotifBinding
-#' @param n number of permutations to run
-#' 
-#' @return a data.frame with numbers of variants with altered motifs and 
-#' adjusted pvalues
+#' Return a contingency table according to direction of test
+#'
+#' @param semScores a SemplScores object
+#' @param d direction of test. Options are: "changed", "gained", "lost"
 #' 
 #' @export
-semEnrichment <- function(sem_scores, n) {
-  permutation_counts <- matrix(0, nrow=nrow(sem_scores@sem_metadata), ncol=16)
-  colnames(permutation_counts) <- c("actual_gained", "perm_gt_gained", 
-                                    "pval_gained", "adjpval_gained",
-                                    "actual_lost", "perm_gt_lost", 
-                                    "pval_lost", "adjpval_lost",
-                                    "actual_changed", "perm_gt_changed",
-                                    "pval_changed", "adjpval_changed",
-                                    "actual_maintained", "perm_gt_maintained", 
-                                    "pval_maintained", "adjpval_maintained")
-  rownames(permutation_counts) <- sem_scores@sem_metadata$sem_id
+enrichment <- function(semScores, d="changed") {
+  s <- scores(semScores)
   
-  permutation_counts <- tallyAlteredMotifs(sem_scores, permutation_counts)
+  fisher_scores <- matrix(numeric(), ncol=6, nrow=length(unique(s$sem)))
+  fisher_scores <- data.table(fisher_scores)
+  colnames(fisher_scores) <- c("sem", "odds.ratio",
+                               "ci.lower", "ci.upper",
+                               "pvalue", "adj.pvalue")
+  fisher_scores$sem <- unique(s$sem)
   
-  motif_count_init <- matrix(0, nrow=nrow(sem_scores@sem_metadata), ncol=4)
-  rownames(motif_count_init) <- sem_scores@sem_metadata$sem_id
-  colnames(motif_count_init) <- c("gained", "lost", "changed", "maintained")
-  
-  num_permutations <- n
-  for (j in 1:num_permutations){
-    # initialize motif counter to store number of variants where each motif is
-    # gained, lost, or maintained
-    motif_count <- motif_count_init
-    for (i in 1:length(sem_scores@variants)){
-      variant_id <- sem_scores@variants$id[i]
-      motif_count <- permuteVariant(sem_scores, motif_count, variant_id)
-    }
+  for (i in 1:nrow(fisher_scores)) {
+    si <- fisher_scores$sem[i]
+    ct <- getContingencyTable(s, d, si)
+    f <- fisher.test(ct)
     
-    # count if number of variants where motif is altered exceeds actual
-    for (sem in rownames(permutation_counts)) {
-      permutation_counts <- countAlteredMotifs(permutation_counts, motif_count, sem)
-    }
+    fisher_scores[i, "pvalue"] <- f$p.value
+    fisher_scores[i, "odds.ratio"] <- f$estimate
+    fisher_scores[i, "ci.lower"] <- f$conf.int[1]
+    fisher_scores[i, "ci.upper"] <- f$conf.int[2]
   }
-  
-  permutation_counts <- calculatePvalues(permutation_counts = permutation_counts, 
-                                         num_permutations = num_permutations)
-  result <- permutation_counts[, c("actual_changed", "adjpval_changed",
-                                   "actual_gained", "adjpval_gained",
-                                   "actual_lost", "adjpval_lost",
-                                   "actual_maintained", "adjpval_maintained")] |>
-    as.data.frame()
-  
-  colnames(result) <- c("changed", "adjpval_changed",
-                        "gained", "adjpval_gained",
-                        "lost", "adjpval_lost",
-                        "maintained", "adjpval_maintained")
-  result <- result[order(result$adjpval_changed, decreasing = FALSE), ]
-  
-  return(result)
+  fisher_scores$adj.pvalue <- p.adjust(fisher_scores$pvalue, 
+                                       method = "BH")
+  return(fisher_scores)
 }
 
