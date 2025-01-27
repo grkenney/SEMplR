914902913fc2aac14ddb67a896199da16a6aa422
Grace Kenney <gracekenney@graces-mbp-226.wireless-1x.unc.edu>
2025-01-27 12:11:30 -0500

    only plot one sequence and remove sequence opacity

--- R/plotMotif.R
+++ R/plotMotif.R
@@ -4,10 +4,9 @@
 #' @param motif sem id to plot. If providing a list of SNPEffectMatrix objects,
 #' must match a name in the list. If providing a single SNPEffectMatrix object,
 #' can provide any character string.
-#' @param refSeq (optional) character sequence of reference
-#' @param altSeq (optional) character sequence of alternate
-#' @param refIndex (optional) index of variant location on reference
-#' @param altIndex (optional) index of variant location on alternate
+#' @param motifSeq (optional) character sequence to color on plot
+#' @param highlight (optional) index of variant location on reference
+#' @param hcol (optional) color of motifSeq basepairs
 #'
 #' @import ggplot2
 #'
@@ -29,21 +28,10 @@
 #' plotMotif(s, semId(s))
 #' 
 #' # color by sequence
-#' plotMotif(s, semId(s), refSeq = "ACT", altSeq = "ATT")
-#' 
-#' # color by sequence and highlight variant position
-#' plotMotif(s, semId(s), refSeq = "ACT", altSeq = "ATT", refIndex = 2)
+#' plotMotif(s, semId(s), motifSeq = "ACT", highlight = 2)
 plotMotif <- function(sems, motif, 
-                      refSeq=NULL, altSeq=NULL, 
-                      refIndex=NULL, altIndex=NULL) {
+                      motifSeq=NULL, highlight=NULL, hcol="dodgerblue") {
   alt <- ref <- NA
-  
-  if (!is.null(refIndex) & !is.null(altIndex)) {
-    if(refIndex != altIndex) {
-      warning("ref and alt variant are located at different motif positions. \
-              Suggest visualizing motifs seperately by sequence.")
-    }
-  }
   
   if (is.list(sems)) {
     sem_baseline <- baseline(sems[[motif]])
@@ -56,78 +44,59 @@ plotMotif <- function(sems, motif,
   # pivoting data longer
   bp <- rep(c("A", "C", "G", "T"), each=nrow(sem_mtx))
   motif_pos <- rep(1:nrow(sem_mtx), 4)
-  pwm_score <- sem_mtx |> 
+  sem_score <- sem_mtx |> 
     unlist()
   
-  sem_mtx_long <- data.table(pwm_score, motif_pos, bp)
+  sem_mtx_long <- data.table(sem_score, motif_pos, bp)
   
-  if (!is.null(refSeq)) {
-    text_color <- "grey"
+  # if sequence is provided for plotting, 
+  if (!is.null(motifSeq)) {
+    mseq <- unlist(strsplit(motifSeq, ""))
     
-    refseq <- unlist(strsplit(refSeq, ""))
+    mseq_dt <- data.table(mseq, 1:nrow(sem_mtx), mseq, "white") |> 
+      stats::setNames(c("bp", "motif_pos", "mseq", "mcol"))
     
-    refseq_dt <- data.table(refseq, 1:nrow(sem_mtx), refseq) |> 
-      stats::setNames(c("bp", "motif_pos", "ref"))
+    sem_mtx_long <- merge(sem_mtx_long, mseq_dt, by=c("bp", "motif_pos"), 
+                          all.x=T)
+   
+    sem_mtx_long$mcol[is.na(sem_mtx_long$mcol)] <- "#9d9d9d"
+    sem_mtx_long$mseq[is.na(sem_mtx_long$mseq)] <- ""
     
-    sem_mtx_long <- merge(sem_mtx_long, refseq_dt, by=c("bp", "motif_pos"), all.x=T)
-    sem_mtx_long[is.na(sem_mtx_long)] <- ""
   } 
   
-  if(!is.null(altSeq)) {    
-    text_color <- "grey"
-
-    altseq <- unlist(strsplit(altSeq, ""))
-    
-    altseq_dt <- data.table(altseq, 1:nrow(sem_mtx), altseq) |> 
-      stats::setNames(c("bp", "motif_pos", "alt"))
-    
-    sem_mtx_long <- merge(sem_mtx_long, altseq_dt, by=c("bp", "motif_pos"), all.x=T)
-    sem_mtx_long[is.na(sem_mtx_long)] <- ""
-    
+  # if aren't plotting specific sequence
+  if(is.null(motifSeq)) {
+    sem_mtx_long$mcol <- "black"
   }
   
-  if(is.null(refSeq) && is.null(altSeq)) {
-    text_color <- "black"
-  }
-  
-  text_size <- 8
+  text_size <- 7
   
   motif_plot <- ggplot2::ggplot(sem_mtx_long, 
-                                aes(x=motif_pos, y=pwm_score))
+                                aes(x=motif_pos, y=sem_score)) +
+    theme_classic()
   
-  if (!is.null(refIndex)) {
-    motif_plot <- motif_plot + geom_vline(xintercept = refIndex, size = 15, 
-                                          col="blue", alpha = 0.1)
-  }
-  
-  if (!is.null(altIndex)) {
-    motif_plot <- motif_plot + geom_vline(xintercept = altIndex, size = 15, 
-                                          col="red", alpha = 0.1)
+  if (!is.null(highlight)) {
+    motif_plot <- motif_plot + geom_vline(xintercept = highlight, size = 15, 
+                                          col=hcol, alpha = 0.1)
   }
   
   motif_plot <- motif_plot + 
     geom_hline(yintercept=0, color = "#eaeaea", size = 1) +
     geom_hline(yintercept=sem_baseline, linetype="dashed", 
                color = "#eaeaea", size = 1) + 
-    geom_text(aes(label=bp), size = text_size, col=text_color) +
+    geom_text(aes(label=sem_mtx_long$bp), size = text_size, col=sem_mtx_long$mcol) +
     scale_x_continuous(breaks = scales::breaks_pretty(n = nrow(sem_mtx))) +
-    theme_classic() +
     theme(legend.position="none") +
     xlab("Position in Motif") +
     ylab("SNP Effect Score") +
     theme(text = element_text(size = 12)) +
     ggtitle(motif)
   
-  if (!is.null(altSeq)) {
-    motif_plot <- motif_plot +
-      geom_text(aes(label=alt), size = text_size, 
-                col="red", alpha=0.6, fontface="bold")
-  }
   
-  if (!is.null(refSeq)) {
+  if (!is.null(motifSeq)) {
     motif_plot <- motif_plot +
-      geom_text(aes(label=ref), size = text_size, 
-                col="blue", alpha=0.5, fontface="bold")
+      geom_text(aes(label=mseq), size = text_size, 
+                col=hcol, fontface="bold")
   }
 
   return(motif_plot)
