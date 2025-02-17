b054242ee45ee4c81ca5fe7619bac7bf134f5133
Grace Kenney <gracekenney@graces-mbp-226.wireless-1x.unc.edu>
2025-02-17 11:20:08 -0500

    label all points and move data prep to it's own function

--- R/plotSemEnrichment.R
+++ R/plotSemEnrichment.R
@@ -1,3 +1,18 @@
+# Add columns with labels for plotting for significant transcription factors
+prepPlotSemEnrichmentDf <- function(e, lab, sigThreshold) {
+  e$lab <- lapply(1:nrow(e), 
+                  function(i) 
+                    ifelse(e$adj.pvalue[i] <= sigThreshold,
+                           as.character(e[[lab]][i]), ""))
+  
+  e$sig <- ifelse(e$adj.pvalue <= sigThreshold, 
+                  paste0("adj. p-value \u2264 ", sigThreshold), 
+                  paste0("> ", sigThreshold))
+  e$sig <- factor(e$sig, levels = c(paste0("adj. p-value \u2264 ", sigThreshold), 
+                                    paste0("> ", sigThreshold)))
+  return(e)
+}
+
 #' Plot motif enrichment results
 #'
 #' @param e SEMplR enrichment results data.table
@@ -14,29 +29,20 @@
 plotSemEnrichment <- function(e, lab = "tf", sigThreshold = 0.05) {
   odds.ratio <- sig <- varId <- NULL
   
-  e$lab <- lapply(1:nrow(e), 
-                  function(i) 
-                    ifelse(e$adj.pvalue[i] < sigThreshold,
-                           as.character(e[[lab]][i]), ""))
-  
-  e$sig <- ifelse(e$adj.pvalue < sigThreshold, 
-                  paste0("adj. p-value \u2264 ", sigThreshold), 
-                  paste0("> ", sigThreshold))
-  e$sig <- factor(e$sig, levels = c(paste0("adj. p-value \u2264 ", sigThreshold), 
-                                    paste0("> ", sigThreshold)))
+  e <- prepPlotSemEnrichmentDf(e, lab, sigThreshold)
   
   p <- ggplot(e, aes(x = stats::reorder(semId, odds.ratio), 
                      y = odds.ratio, col=sig)) + 
     geom_point() +
     theme_classic() +
-    ggrepel::geom_text_repel(aes(semId, odds.ratio, 
-                                 label = lab), size = 3, show.legend = F) +
+    ggrepel::geom_text_repel(aes(semId, odds.ratio, label = lab), 
+                             size = 3, show.legend = F, max.overlaps = Inf) +
     guides(colour  = guide_legend(position = "inside")) +
     theme(axis.text.x=element_blank(),
           axis.ticks.x=element_blank(),
           legend.title = element_blank(),
           legend.text = element_text(size=10),
-          legend.position.inside = c(0.15, 0.95),
+          legend.position = c(0.15, 0.95),
           legend.background = element_rect(fill=alpha('white', 0)),
           legend.key = element_rect(fill = "transparent", colour = "transparent")) +
     scale_color_manual(values = c("dodgerblue", "grey"),
