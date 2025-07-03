f514b7919cf92ec236ed28bedbb4dfdc908f5f11
Grace Kenney <kenney.grace6@gmail.com>
2025-07-03 13:35:28 -0400

    move convertSEMsToPPMs function to it's own script

--- R/enrichSEMs.R
+++ R/enrichSEMs.R
@@ -25,36 +25,6 @@
 }
 
 
-#' Convert a SNP Effect Matrix to a position probability matrices (PPMs)
-#' 
-#' Converts the SNP Effect Matrix in a SNPEffectMatrix or 
-#' SNPEffectMatrixCollection object to a position probability matrix (PPM)
-#' where each entry is the probability of a base at each position in the motif.
-#'
-#' @param x A `SNPEffectMatrix` or `SNPEffectMatrixCollection` object
-#' 
-#' @importFrom methods is
-#'
-#' @return a `list` of matrices
-#'
-#' @export
-convertSEMsToPPMs <- \(x) {
-  if(is(x)[1] == "SNPEffectMatrixCollection") {
-    ss <- sems(x)
-  } else if (is(x)[1] == "SNPEffectMatrix") {
-    ss <- list(x)
-  } else if (is(x)[1] == "list" & is(x[[1]]) == "SNPEffectMatrix") {
-    ss <- x
-  } else {
-    stop("x must be of class SNPEffectMatrixCollection or SNPEffectMatrix or
-         a list of SNPEffectMatrix objects")
-  }
-  
-  ppms <- lapply(ss, function(s) .semToPpm(s))
-  return(ppms)
-}
-
-
 #' Calculates binding enrichment for motif(s)
 #' 
 #' Perform a binomial test to determine if SNP Effect Matrices are bound more
