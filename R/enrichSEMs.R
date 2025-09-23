16c4f65b99822d5a573b491a578ca83091052e7a
Grace Kenney <kenney.grace6@gmail.com>
2025-09-22 21:58:18 -0400

    add examples

--- R/enrichSEMs.R
+++ R/enrichSEMs.R
@@ -1,64 +1,81 @@
-# .scrambleFlanks <- function(seqs) {
-#   prefix <- substr(seqs, 1, nchar(seqs)/2 - 0.5)
-#   suffix <- substr(seqs, nchar(prefix)+2, nchar(seqs))
-#   allele <- substr(seqs, nchar(prefix)+1, nchar(prefix)+1)
-#   
-#   flank_concat <- lapply(1:length(seqs), 
-#                          function(i) stringi::stri_c(prefix[i], suffix[i]))
-#   scrambled <- lapply(flank_concat, 
-#                       function(x) stringi::stri_rand_shuffle(x)) |> unlist()
-#   new_prefix <- substr(scrambled, 1, nchar(prefix))
-#   new_suffix <- substr(scrambled, nchar(prefix)+1, nchar(seqs)-1)
-#   
-#   scrambled_seqs <- stringi::stri_c(new_prefix, allele, new_suffix)
-#   return(scrambled_seqs)
-# }
-
-
 # given a list of DNA sequences, scramble the entire sequence
 .scrambleSeqs <- function(seqs) {
-  scrambled <- lapply(seqs, 
-                      function(x) stringi::stri_rand_shuffle(x)) |> unlist()
-  return(scrambled)
+    scrambled <- lapply(
+        seqs,
+        function(x) stringi::stri_rand_shuffle(x)
+    ) |> unlist()
+    return(scrambled)
 }
 
 
-# perform a binomial test on sem scores versus a scores from a background for a 
+# perform a binomial test on sem scores versus a scores from a background for a
 # single sem
 .binomSEMEnrich <- \(xs, bg, semName) {
-  xs_sem <- xs[xs$SEM == semName, ]
-  bg_sem <- bg[bg$SEM == semName, ]
-  
-  successes <- sum(xs_sem$scoreNorm > 0)
-  sample_size <- nrow(xs_sem)
-  prob_success <- (sum(bg_sem$scoreNorm > 0) + 1) / nrow(bg_sem)
-  
-  if(prob_success > 1) {
-    prob_success <- 1
-  }
-  
-  binom_test_res <- stats::binom.test(x = successes, 
-                                      n = sample_size, 
-                                      p = prob_success, 
-                                      alternative = "greater")
-  
-  n_bound_bg <- sum(bg$scoreNorm[bg$SEM == semName] > 0)
-  result <- data.table::data.table(SEM = semName,
-                                   pvalue = binom_test_res$p.value,
-                                   n_bound = successes,
-                                   n_bound_bg = n_bound_bg)
-  return(result)
+    xs_sem <- xs[xs$SEM == semName, ]
+    bg_sem <- bg[bg$SEM == semName, ]
+
+    successes <- sum(xs_sem$scoreNorm > 0)
+    sample_size <- nrow(xs_sem)
+    prob_success <- (sum(bg_sem$scoreNorm > 0) + 1) / nrow(bg_sem)
+
+    if (prob_success > 1) {
+        prob_success <- 1
+    }
+
+    binom_test_res <- stats::binom.test(
+        x = successes,
+        n = sample_size,
+        p = prob_success,
+        alternative = "greater"
+    )
+
+    n_bound_bg <- sum(bg$scoreNorm[bg$SEM == semName] > 0)
+    result <- data.table::data.table(
+        SEM = semName,
+        pvalue = binom_test_res$p.value,
+        n_bound = successes,
+        n_bound_bg = n_bound_bg
+    )
+    return(result)
+}
+
+
+# define the background set if not provided
+.defineBackground <- \(x, sem, background, seqs, nFlank, genome) {
+    if (is.null(background)) {
+        if (is(x, "SEMplScores")) {
+            seqs <- getRanges(x)$sequence
+        }
+
+        scramb <- .scrambleSeqs(seqs)
+        bg <- lapply(
+            sems(sem),
+            function(y) {
+                scoreSequence(
+                    sem = as.matrix(getSEM(y)),
+                    dna_sequences = scramb,
+                    nFlank = nFlank,
+                    bl = getBaseline(y),
+                    seqIds = seq_along(seqs)
+                )
+            }
+        ) |> data.table::rbindlist(idcol = "SEM")
+    } else {
+        bg <- scoreBinding(x = background, sem = sem, genome = genome) |>
+            scores()
+    }
+    return(bg)
 }
 
 
 #' Calculates binding enrichment for motif(s)
-#' 
+#'
 #' Perform a binomial test to determine if SNP Effect Matrices are bound more
 #' often than expected.
 #'
 #' @param x The scoring table produced by `scoreBinding`
 #' @param sem A `SNPEffectMatrix` or `SNPEffectMatrixCollection` object
-#' @param background A list of DNA sequences to use for background. The length 
+#' @param background A list of DNA sequences to use for background. The length
 #' of each sequence must match the length of sequences in `x`. By default, will
 #' scramble the provided sequences.
 #' @param seqs The sequences scored in `scoreBinding`
@@ -69,38 +86,51 @@
 #'
 #' @return a `list` of matrices
 #'
+#' @examples
+#' 
+#' # load SEMs
+#' data(sc)
+#'
+#' # note that this is a small example for demonstration purposes
+#' # in actual enrichment analyses sets of 100+ ranges are recommended
+#' 
+#' # create a GRanges object
+#' gr <- GenomicRanges::GRanges(
+#'     seqnames = "chr12",
+#'     ranges = 94136009
+#' )
+#'
+#' # calculate binding propensity
+#' sb <- scoreBinding(gr, sc, BSgenome.Hsapiens.UCSC.hg19::Hsapiens)
+#' 
+#' enrichSEMs(sb, sc)
+#'
 #' @export
-enrichSEMs <- \(x, sem, 
-                background = NULL, seqs = NULL, nFlank = 0,
-                genome = NULL) {
-  sem_names <- sems(sem) |> names()
+enrichSEMs <- \(x, sem,
+    background = NULL, seqs = NULL, nFlank = 0,
+    genome = NULL) {
+    sem_names <- sems(sem) |> names()
 
-  if (is.null(background)) {
-    if (is(x, "SEMplScores")) {
-      seqs <- getRanges(x)$sequence
-    }
-    
-    scramb <- .scrambleSeqs(seqs)
-    
-    bg <- lapply(sems(sem), 
-                 function(y) scoreSequence(sem = as.matrix(getSEM(y)), 
-                                           dna_sequences = scramb, 
-                                           nFlank = nFlank, 
-                                           bl = getBaseline(y),
-                                           seqIds = seq_along(seqs))) |>
-      data.table::rbindlist(idcol = "SEM")
-  } else {
-    bg <- scoreBinding(x = background, sem = sem, genome = genome) |> 
-      scores()
-  }
-  
-  x_scores <- scores(x)
-  result <- lapply(seq_along(sem_names), 
-                   \(i) .binomSEMEnrich(xs = x_scores, 
-                                        bg = bg,
-                                        semName = sem_names[i])) |>
-    data.table::rbindlist()
-  
-  result$padj <- stats::p.adjust(result$pvalue, method = "BH")
-  return(result)
+    bg <- .defineBackground(
+        x = x,
+        sem = sem,
+        background = background,
+        seqs = seqs,
+        nFlank = nFlank,
+        genome = genome
+    )
+
+    x_scores <- scores(x)
+    result <- lapply(
+        seq_along(sem_names),
+        \(i) .binomSEMEnrich(
+            xs = x_scores,
+            bg = bg,
+            semName = sem_names[i]
+        )
+    ) |>
+        data.table::rbindlist()
+
+    result$padj <- stats::p.adjust(result$pvalue, method = "BH")
+    return(result)
 }
--- R/plotEnrich.R
+++ R/plotEnrich.R
@@ -1,112 +1,148 @@
 # convert SEM to Motif
 .formatMotifs <- \(sem, label) {
-  .SD <- NULL
-  ppms <- convertSEMsToPPMs(sems(sem))
-  motifs <- lapply(seq_along(ppms), 
-                   function(i) 
-                     universalmotif::create_motif(ppms[[i]], 
-                                                  name = names(ppms[i])))
-  
-  for (i in seq_along(motifs)) {
-    motifs[[i]]["altname"] <- semData(sem)[motifs[[i]]["name"], 
-                                           .SD, .SDcols = label] |>
-      unlist() |>
-      unname()
-  }
-  return(motifs)
+    .SD <- NULL
+    ppms <- convertSEMsToPPMs(sems(sem))
+    motifs <- lapply(
+        seq_along(ppms),
+        function(i) {
+            universalmotif::create_motif(ppms[[i]],
+                name = names(ppms[i])
+            )
+        }
+    )
+
+    for (i in seq_along(motifs)) {
+        motifs[[i]]["altname"] <- semData(sem)[motifs[[i]]["name"],
+            .SD,
+            .SDcols = label
+        ] |>
+            unlist() |>
+            unname()
+    }
+    return(motifs)
 }
 
 
 # construct a matrix comparing similarity of each motif
 .constructComparisons <- \(motifs, labels, method) {
-  comparisons <- universalmotif::compare_motifs(motifs, 
-                                                method = method, 
-                                                min.mean.ic = 0)
-  colnames(comparisons) <- labels
-  rownames(comparisons) <- labels
-  
-  comparisons <- 1 - comparisons
-  comparisons <- stats::as.dist(comparisons)
-  
-  comparisons <- ape::as.phylo(stats::hclust(comparisons))
-  return(comparisons)
+    comparisons <- universalmotif::compare_motifs(motifs,
+        method = method,
+        min.mean.ic = 0
+    )
+    colnames(comparisons) <- labels
+    rownames(comparisons) <- labels
+
+    comparisons <- 1 - comparisons
+    comparisons <- stats::as.dist(comparisons)
+
+    comparisons <- ape::as.phylo(stats::hclust(comparisons))
+    return(comparisons)
 }
 
 
 # construct phylogenetic tree of motif similarity
 .plotMotifTree <- \(e, sem, threshold, comparisons, sigCol) {
-  sm <- NA
-  # identify significant motifs
-  sig_motifs <- e$SEM[e$padj <= threshold]
-  
-  # store TF names for significant motifs for labeling
-  sm_tf <- unlist(semData(sem)[sig_motifs, "transcription_factor"])
-  comparisons <- ggtree::groupOTU(comparisons,
-                                  sm_tf,
-                                  group_name = "sm")
-  
-  tree <- ggtree::ggtree(comparisons, layout = "fan") +
-    theme(legend.position = "", legend.title = element_blank()) +
-    theme(plot.margin = margin(10, 10, 10, 100)) + 
-    ggtree::geom_tiplab(aes(colour = sm), 
-                        as_ylab = FALSE, 
-                        size = 1.5, 
-                        offset = .05) +
-    scale_color_manual(values=c("grey", sigCol), guide="none")
-  return(tree)
+    sm <- NA
+    # identify significant motifs
+    sig_motifs <- e$SEM[e$padj <= threshold]
+
+    # store TF names for significant motifs for labeling
+    sm_tf <- unlist(semData(sem)[sig_motifs, "transcription_factor"])
+    comparisons <- ggtree::groupOTU(comparisons,
+        sm_tf,
+        group_name = "sm"
+    )
+
+    tree <- ggtree::ggtree(comparisons, layout = "fan") +
+        theme(legend.position = "", legend.title = element_blank()) +
+        theme(plot.margin = margin(10, 10, 10, 100)) +
+        ggtree::geom_tiplab(aes(colour = sm),
+            as_ylab = FALSE,
+            size = 1.5,
+            offset = .05
+        ) +
+        scale_color_manual(values = c("grey", sigCol), guide = "none")
+    return(tree)
 }
 
 
 # add heatmap to ggtree object
 .plotTreeHeatmap <- \(plt, e, labels) {
-  heatmapData <- e[, "padj"] |> as.data.frame()
-  colnames(heatmapData) <- "-log10(padj)"
-  rownames(heatmapData) <- labels
-  
-  plt <- ggtree::gheatmap(plt, -log10(heatmapData), offset=-0.014, 
-                          width=0.12, colnames = FALSE,
-                          font.size = 2.5)
-  # add the scale bar
-  plt <- plt +
-    theme(legend.position = "bottom", 
-          legend.title = element_text(color = "black"),
-          legend.title.position = "top") +
-    scale_fill_viridis_c(na.value = "white", direction = -1, 
-                         name = "-log10(padj)")
-  return(plt)
+    heatmapData <- e[, "padj"] |> as.data.frame()
+    colnames(heatmapData) <- "-log10(padj)"
+    rownames(heatmapData) <- labels
+
+    plt <- ggtree::gheatmap(plt, -log10(heatmapData),
+        offset = -0.014,
+        width = 0.12, colnames = FALSE,
+        font.size = 2.5
+    )
+    # add the scale bar
+    plt <- plt +
+        theme(
+            legend.position = "bottom",
+            legend.title = element_text(color = "black"),
+            legend.title.position = "top"
+        ) +
+        scale_fill_viridis_c(
+            na.value = "white", direction = -1,
+            name = "-log10(padj)"
+        )
+    return(plt)
 }
 
 
 #' Plot the results of `enrichSEMs`
-#' 
-#' Generates a cladogram clustered by SNP Effect Matrix similarity and 
+#'
+#' Generates a cladogram clustered by SNP Effect Matrix similarity and
 #' a heatmap representing the adjusted p-value of the enrichment.
 #'
 #' @param e The resulting data.table from `enrichSEMs`
 #' @param sem A `SNPEffectMatrixCollection` object
 #' @param label Column in semData(sem) to use for tree labels
-#' @param method Method to use for SEM comparison. 
+#' @param method Method to use for SEM comparison.
 #' See ?universalmotif::compare_motifs for options.
 #' @param threshold The adjusted p-value threshold for coloring SEMs
 #' @param sigCol Color to label significant SEM labels
 #'
 #' @return a `ggtree` object
 #'
+#' @examples
+#' # load SEMs
+#' data(sc)
+#'
+#' # note that this is a small example for demonstration purposes
+#' # in actual enrichment analyses sets of 100+ ranges are recommended
+#' 
+#' # create a GRanges object
+#' gr <- GenomicRanges::GRanges(
+#'     seqnames = "chr12",
+#'     ranges = 94136009
+#' )
+#'
+#' # calculate binding propensity
+#' sb <- scoreBinding(gr, sc, BSgenome.Hsapiens.UCSC.hg19::Hsapiens)
+#' 
+#' e <- enrichSEMs(sb, sc) 
+#' # plotEnrich(e, sc)
+#'
 #' @export
-plotEnrich <- \(e, sem, 
-                label = "transcription_factor", 
-                method = "WPCC", 
-                threshold = 0.05,
-                sigCol = "purple4") {
-  motifs <- .formatMotifs(sem, label)
-  labels <- lapply(motifs, function(x) x["altname"]) |> unlist()
-  
-  comparisons <- .constructComparisons(motifs, labels, method = method)
-  
-  plt <- .plotMotifTree(e = e, sem = sem, threshold = threshold,
-                        comparisons = comparisons, sigCol = sigCol)
-  
-  plt <- .plotTreeHeatmap(plt = plt, e = e, labels = labels)
-    
-  return(plt)
+plotEnrich <- \(e, sem,
+    label = "transcription_factor",
+    method = "WPCC",
+    threshold = 0.05,
+    sigCol = "purple4") {
+    motifs <- .formatMotifs(sem, label)
+    labels <- lapply(motifs, function(x) x["altname"]) |> unlist()
+
+    comparisons <- .constructComparisons(motifs, labels, method = method)
+
+    plt <- .plotMotifTree(
+        e = e, sem = sem, threshold = threshold,
+        comparisons = comparisons, sigCol = sigCol
+    )
+
+    plt <- .plotTreeHeatmap(plt = plt, e = e, labels = labels)
+
+    return(plt)
 }
